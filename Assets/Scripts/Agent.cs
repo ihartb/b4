9eefd811317ff275babc60048c50439a5f970e9b
Samuel S. Sohn <SSSohn@users.noreply.github.com>
2020-03-28 01:09:00 -0400

    ready for release

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -1,17 +1,107 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Agent : MonoBehaviour
 {
+    private List<Vector3> path;
+    private NavMeshAgent nma;
+    private Rigidbody rb;
+
+    private HashSet<GameObject> perceivedNeighbors = new HashSet<GameObject>();
 
     void Start()
     {
-        
+        path = new List<Vector3>();
+        nma = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    private void Update()
+    {
+        if (path.Count > 0 && Vector3.Distance(transform.position, path[0]) < 1.25f)
+        {
+            path.RemoveAt(0);
+
+            if (path.Count == 0)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    #region Public Functions
+
+    public void ComputePath(Vector3 destination)
+    {
+        nma.enabled = true;
+        var nmPath = new NavMeshPath();
+        nma.CalculatePath(destination, nmPath);
+        path = nmPath.corners.Skip(1).ToList();
+        //path = new List<Vector3>() { destination };
+        //nma.SetDestination(destination);
+        nma.enabled = false;
+    }
+
+    #endregion
+
+    #region Incomplete Functions
+
+    private Vector3 ComputeForce()
+    {
+        if (path.Count == 0)
+        {
+            return Vector3.zero;
+        }
+
+        var temp = path[0] - transform.position;
+        return temp.normalized * Mathf.Min(temp.magnitude, 1f);
+    }
+
+    private Vector3 CalculateGoalForce()
+    {
+        return Vector3.zero;
+    }
+
+    private Vector3 CalculateAgentForce()
+    {
+        return Vector3.zero;
+    }
+
+    private Vector3 CalculateWallForce()
+    {
+        return Vector3.zero;
+    }
+
+    public void ApplyForce()
+    {
+        var force = ComputeForce();
+        force.y = 0;
+
+        rb.AddForce(force * 10, ForceMode.Force);
+    }
+
+    public void OnTriggerEnter(Collider other)
     {
         
     }
+
+    public void OnTriggerExit(Collider other)
+    {
+        
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {
+        
+    }
+
+    #endregion
 }
--- Assets/Scripts/AgentManager.cs
+++ Assets/Scripts/AgentManager.cs
@@ -12,8 +12,9 @@ public class AgentManager : MonoBehaviour
 
     private List<Agent> agents = new List<Agent>();
     private GameObject agentParent;
+    private static HashSet<GameObject> agentsObjs = new HashSet<GameObject>();
 
-    private const float UPDATE_RATE = 0.5f;
+    private const float UPDATE_RATE = 0.0f;
 
     #region Unity Functions
 
@@ -24,10 +25,10 @@ public class AgentManager : MonoBehaviour
         agentParent = GameObject.Find("Agents");
         for (int i = 0; i < agentCount; i++)
         {
-            var randPos = new Vector3((Random.value - 0.5f) * agentSpawnRadius, 1, (Random.value - 0.5f) * agentSpawnRadius);
+            var randPos = new Vector3((Random.value - 0.5f) * agentSpawnRadius, 0, (Random.value - 0.5f) * agentSpawnRadius);
             NavMeshHit hit;
             NavMesh.SamplePosition(randPos, out hit, 10, NavMesh.AllAreas);
-            randPos = hit.position;
+            randPos = hit.position + Vector3.up;
 
             GameObject agent = null;
             agent = Instantiate(agentPrefab, randPos, Quaternion.identity);
@@ -36,6 +37,7 @@ public class AgentManager : MonoBehaviour
             var agentScript = agent.GetComponent<Agent>();
 
             agents.Add(agentScript);
+            agentsObjs.Add(agent);
         }
 
         StartCoroutine(Run());
@@ -45,11 +47,38 @@ public class AgentManager : MonoBehaviour
     {
         #region Visualization
         
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (true)
+            {
+                var point = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 10);
+                var dir = point - Camera.main.transform.position;
+                RaycastHit rcHit;
+                if (Physics.Raycast(point, dir, out rcHit))
+                {
+                    point = rcHit.point;
+
+                    SetAgentDestinations(point);
+                }
+            } else
+            {
+                var randPos = new Vector3((Random.value - 0.5f) * agentSpawnRadius, 0, (Random.value - 0.5f) * agentSpawnRadius);
+
+                NavMeshHit hit;
+                NavMesh.SamplePosition(randPos, out hit, 1.0f, NavMesh.AllAreas);
+                print(hit.position);
+                Debug.DrawLine(hit.position, hit.position + Vector3.up * 10, Color.red, 1000000);
+                foreach (var agent in agents)
+                {
+                    //agent.ComputePath(hit.position);
+                }
+            }
+        }
 
 #if UNITY_EDITOR
         if (Application.isFocused)
         {
-            UnityEditor.SceneView.FocusWindowIfItsOpen(typeof(UnityEditor.SceneView));
+            //UnityEditor.SceneView.FocusWindowIfItsOpen(typeof(UnityEditor.SceneView));
         }
 #endif
 
@@ -62,7 +91,10 @@ public class AgentManager : MonoBehaviour
 
         while (true)
         {
-            
+            foreach (var agent in agents)
+            {
+                agent.ApplyForce();
+            }
 
             yield return new WaitForSeconds(UPDATE_RATE);
         }
@@ -70,20 +102,35 @@ public class AgentManager : MonoBehaviour
 
     #endregion
 
-    #region Incomplete Functions
-    
+    #region Public Functions
+
+    public static bool IsAgent(GameObject obj)
+    {
+        return agentsObjs.Contains(obj);
+    }
+
+    public void SetAgentDestinations(Vector3 destination)
+    {
+        NavMeshHit hit;
+        NavMesh.SamplePosition(destination, out hit, 10, NavMesh.AllAreas);
+        foreach (var agent in agents)
+        {
+            agent.ComputePath(hit.position);
+        }
+    }
+
     #endregion
 
     #region Private Functions
-    
+
     #endregion
 
     #region Visualization Functions
-    
+
     #endregion
 
     #region Utility Classes
-    
+
     private class Tuple<K,V>
     {
         public K Item1;
