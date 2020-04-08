b0669a8b9cf96ed5aa578e091c88971ed21a0b38
Samuel S. Sohn <SSSohn@users.noreply.github.com>
2020-04-08 00:33:09 -0400

    Updated WallManager

--- Assets/Scripts/Parameters.cs
+++ Assets/Scripts/Parameters.cs
@@ -10,5 +10,10 @@ public struct Parameters
     public const float k = 1.2f * 100000f;
     public const float Kappa = 2.4f * 100000f;
 
+    public const float WALL_A = 2000f;
+    public const float WALL_B = 0.08f;
+    public const float WALL_k = 1.2f * 100000f;
+    public const float WALL_Kappa = 2.4f * 100000f;
+
     public const float maxSpeed = 100;
 }
--- Assets/Scripts/WallManager.cs
+++ Assets/Scripts/WallManager.cs
@@ -32,6 +32,34 @@ public class WallManager : MonoBehaviour
                     wallObjs.Add(wall);
                 }
             }
+        for (int i = -Mathf.RoundToInt(wallSpawnRadius / 2); i < wallSpawnRadius / 2; i++)
+        {
+            foreach (var j in new[] { -Mathf.RoundToInt(wallSpawnRadius / 2) - 1, wallSpawnRadius / 2 })
+            {
+                GameObject wall = null;
+                wall = Instantiate(wallPrefab, new Vector3(i + 0.5f, 0.5f, j + 0.5f), Quaternion.identity);
+                wall.name = "Wall " + i;
+                wall.transform.parent = wallParent.transform;
+                var wallScript = wall.GetComponent<Agent>();
+
+                walls.Add(wallScript);
+                wallObjs.Add(wall);
+            }
+        }
+        for (int j = -Mathf.RoundToInt(wallSpawnRadius / 2); j < wallSpawnRadius / 2; j++)
+        {
+            foreach (var i in new[] { -Mathf.RoundToInt(wallSpawnRadius / 2) - 1, wallSpawnRadius / 2 })
+            {
+                GameObject wall = null;
+                wall = Instantiate(wallPrefab, new Vector3(i + 0.5f, 0.5f, j + 0.5f), Quaternion.identity);
+                wall.name = "Wall " + i;
+                wall.transform.parent = wallParent.transform;
+                var wallScript = wall.GetComponent<Agent>();
+
+                walls.Add(wallScript);
+                wallObjs.Add(wall);
+            }
+        }
     }
 
     void Update()
